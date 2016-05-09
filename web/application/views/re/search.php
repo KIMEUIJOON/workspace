ec8c0d70b8cd12eee9f8e2ae2dbbfe4c5490facb
KIMEUIJOON <ech2357@gmail.com>
2016-05-10 00:04:58 +0900

    0510/00:04

--- web/application/views/re/search.php
+++ web/application/views/re/search.php
@@ -18,67 +18,70 @@ load->view('re/common/area_top') ?>
 			<div class="centered">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<!--top_panel start -->
+					<?= $this->
+					load->view('re/search/searchPanel') ?>
 					<div class="padding">
-						<table style="width:100%">
-						<tr>
-							<td>
-								<b>キャリア(通信業者)</b>
-							</td>
-							<td>
-								<select name="carrier" class="search_text_border">
-									<option value="no" selected="selected">キャリアを選択してください</option>
-									<option value="docomo">docomo</option>
-									<option value="au">au</option>
-									<option value="softbank">softbank</option>
-									<option value="ymobile">ymobile</option>
-									<option value="total">トータルショップ</option>
-								</select>
-							</td>
-							<td>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<label>フリーワード</label>
-							</td>
-							<td>
-								<input type="text" class="search_text_border">
-							</td>
-							<td>
-								<label>＆その他の検索項目</label>
-							</td>
-						</tr>
-						</table>
-						<br/>
-						<table style="width:100%;">
-						<tr>
-							<td>
-								<input type="checkbox" name="other_choice" value="parking">駐車場あり
-							</td>
-							<td>
-								<input type="checkbox" name="other_choice" value="kid-room">キッズルーム
-							</td>
-							<td>
-								<input type="checkbox" name="other_choice" value="holds-rates">料金収納可
-							</td>
-							<td>
-								<input type="checkbox" name="other_choice" value="after21">21時以降も営業
-							</td>
-							<td>
-								<input type="checkbox" name="other_choice" value="barrier-free">バリアフリー
-							</td>
-							<td>
-								<input type="checkbox" name="other_choice" value="study-smartphone">ケータイ教室
-							</td>
-							<td>
-								<input type="checkbox" name="other_choice" value="breakdown">故障受付可
-							</td>
-							<td>
-								<img src="http:\\localhost\src\re\img\pc\button_search.png">
-							</td>
-						</tr>
-						</table>
+					  <table style="width:100%">
+					  <tr>
+					    <td>
+					      <b>キャリア(通信業者)</b>
+					    </td>
+					    <td>
+					      <select name="carrier" class="search_text_border">
+					        <option value="no" selected="selected">キャリアを選択してください</option>
+					        <option value="docomo">docomo</option>
+					        <option value="au">au</option>
+					        <option value="softbank">softbank</option>
+					        <option value="ymobile">ymobile</option>
+					        <option value="total">トータルショップ</option>
+					      </select>
+					    </td>
+					    <td>
+					    </td>
+					  </tr>
+					  <tr>
+					    <td>
+					      <label>フリーワード</label>
+					    </td>
+					    <td>
+					      <input type="text" class="search_text_border">
+					    </td>
+					    <td>
+					      <label>＆その他の検索項目</label>
+					    </td>
+					  </tr>
+					  </table>
+					  <br/>
+					  <table style="width:100%;">
+					  <tr>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="parking">駐車場あり
+					    </td>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="kid-room">キッズルーム
+					    </td>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="holds-rates">料金収納可
+					    </td>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="after21">21時以降も営業
+					    </td>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="barrier-free">バリアフリー
+					    </td>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="study-smartphone">ケータイ教室
+					    </td>
+					    <td>
+					      <input type="checkbox" name="other_choice" value="breakdown">故障受付可
+					    </td>
+					    <td>
+					      <img src="http:\\localhost\src\re\img\pc\button_search.png">
+					    </td>
+					  </tr>
+					  </table>
 					</div>
+
 					<!--top_panel end -->
 					<!--area_各店舗 start -->
 					<div class="padding">
---
+++ web/application/views/re/search/searchPanel.php
@@ -0,0 +1,86 @@
+<div class="back_gray">
+  <table>
+<!--프리키워드 -->
+    <tr>
+      <td>
+        フリーキーワード
+      </td>
+
+    </tr>
+<!--캐리어 -->
+    <tr>
+      <td>
+        キャリア
+      </td>
+      <td>
+        <div class="text-center">
+          <div class="row">
+            <img class="img-responsive col-lg-3 col-md-3 col-sm-3 col-xs-3" name="carrier" value="searchPanel_docomo" check="off"
+            src="http:\\localhost\src\re\img\pc\sideLogo_docomo.png" onclick="checkbox_select(this);">
+            <img class="img-responsive col-lg-3 col-md-3 col-sm-3 col-xs-3" name="carrier" value="searchPanel_au" check="off"
+            src="http:\\localhost\src\re\img\pc\sideLogo_au.png" onclick="checkbox_select(this);">
+            <img class="img-responsive col-lg-3 col-md-3 col-sm-3 col-xs-3" name="carrier" value="searchPanel_softbank" check="off"
+            src="http:\\localhost\src\re\img\pc\sideLogo_softbank.png" onclick="checkbox_select(this);">
+            <img class="img-responsive col-lg-3 col-md-3 col-sm-3 col-xs-3" name="carrier" value="searchPanel_ymobile" check="off"
+            src="http:\\localhost\src\re\img\pc\sideLogo_ymobile.png" onclick="checkbox_select(this);">
+        </div>
+          <div class="hiddenCheckbox">
+          <input type="checkbox" name="carrier" value="docomo" id="searchPanel_docomo">
+          <input type="checkbox" name="carrier" value="au" id="searchPanel_au">
+          <input type="checkbox" name="carrier" value="softbank" id="searchPanel_softbank">
+          <input type="checkbox" name="carrier" value="ymobile" id="searchPanel_ymobile">
+          </div>
+        </ul>
+      </div>
+      </td>
+    </tr>
+<!--지역 -->
+    <tr>
+      <td>
+        地域
+      </td>
+    </tr>
+<!--체크박스 -->
+    <tr>
+      <td>
+        サービス
+      </td>
+      <td>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 searchPanel_checkbox">
+            <div>
+            <img name="other_choice" value="side_navi_choice_parking" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> 駐車場あり</div>
+            <div>
+            <img name="other_choice" value="side_navi_choice_kid-room" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> キッズルーム</div>
+            <div>
+            <img name="other_choice" value="side_navi_choice_holds-rates" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> 料金収納可</div>
+            <div>
+            <img name="other_choice" value="side_navi_choice_after21" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> 21時以降も営業</div>
+
+            <div>
+            <img name="other_choice" value="side_navi_choice_barrier-free" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> バリアフリー</div>
+            <div>
+            <img name="other_choice" value="side_navi_choice_study-smartphone" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> ケータイ教室</div>
+            <div>
+            <img name="other_choice" value="side_navi_choice_breakdown" check="off"
+            src="http:\\localhost\src\re\img\pc\button_checkBox.png" onclick="checkbox_select(this);"> 故障受付可</div>
+
+          <div class="hiddenCheckbox">
+                <input type="checkbox" name="other_choice" value="parking" id="side_navi_choice_parking">駐車場あり
+                <input type="checkbox" name="other_choice" value="kid-room" id="side_navi_choice_kid-room">キッズルーム
+                <input type="checkbox" name="other_choice" value="holds-rates" id="side_navi_choice_holds-rates">料金収納可
+                <input type="checkbox" name="other_choice" value="after21" id="side_navi_choice_after21">21時以降も営業
+                <input type="checkbox" name="other_choice" value="barrier-free" id="side_navi_choice_barrier-free">バリアフリー
+                <input type="checkbox" name="other_choice" value="study-smartphone" id="side_navi_choice_study-smartphone">ケータイ教室
+                <input type="checkbox" name="other_choice" value="breakdown" id="side_navi_choice_breakdown">故障受付可
+          </div>
+        </div>
+      </td>
+    </tr>
+  </table>
+</div>
